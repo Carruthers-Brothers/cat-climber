73362037101a4e5a5f6b0a5882c214691eb398f8
Cameron Carruthers <cameronlcarruthers@gmail.com>
2024-08-17 21:36:06 -0600

    feat: Have heart beat on pulse

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -6,3 +6,13 @@ func _ready():
 
 func load_hearts():
 	$PawPrint.size.x = Global.lives * 1218
+
+func _on_conductor_beat(song_position_beat):
+	$Fish.scale.x = $Fish.scale.x * 1.1
+	$Fish.scale.y = $Fish.scale.y * 1.1
+	
+	$Timer.start()
+	
+func _on_timer_timeout():
+	$Fish.scale.x = $Fish.scale.x / 1.1
+	$Fish.scale.y = $Fish.scale.y / 1.1
