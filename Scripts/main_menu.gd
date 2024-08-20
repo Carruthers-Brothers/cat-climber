352a553fa14ac88b0e456bb138945440d063f96c
sam-carruthers99 <samcarruthers99@gmail.com>
2024-08-20 11:14:43 -0600

    backgrounds

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -3,9 +3,9 @@ extends Control
 @onready var settings_menu = $MarginContainer/SettingsMenu
 @onready var tutorial_menu = $TutorialMenu
 
-
 func _on_play_button_pressed():
 	# get_tree().change_scene_to_file("res://Scenes/game.tscn") # to skip calibration
+	# get_tree().change_scene_to_packed(GAME)
 	tutorial_menu.show()
 
 
